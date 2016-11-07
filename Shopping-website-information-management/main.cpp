1458a998bc73aae77bcba2faa468d45d6e5a375a
LogicJake <835410808@qq.com>
2016-11-07 14:33:24 +0800

    Update
    实现商铺信息的链式存储，完成增加商铺功能，修改信息

--- Shopping-website-information-management/main.cpp
+++ Shopping-website-information-management/main.cpp
@@ -6,6 +6,8 @@
 #define FALSE 0
 #define OK 1
 #define ERROR 0
+#define goodsNumber 6
+#define ShopNumber 16
 using namespace std;
 typedef struct Items
 {
@@ -18,28 +20,32 @@ typedef struct Shop
 	int number;
 	char ShopName[20];
 	int CreditWorthiness;
-	int goods[6];		//商品编码
+	Items goods[6];
 }Shop;
 typedef Shop ElemType;
-istream& operator>>(istream& in, Items& s)
-{
-    in>>s.ItemsName>>s.price>>s.Sales;
-    return in;
-}
-ostream& operator<<(ostream& out, const Items& s)
-{
-    out<<s.ItemsName<<" "<<s.price<<" "<<s.Sales<<endl; 
-    return out;
-}
+ istream& operator>>(istream& in, Items& s)
+ {
+     in>>s.ItemsName>>s.price>>s.Sales;
+     return in;
+ }
+ ostream& operator<<(ostream& out, const Items& s)
+ {
+     out<<s.ItemsName<<" "<<s.price<<" "<<s.Sales<<" "; 
+     return out;
+ }
 istream& operator>>(istream& in, Shop& s)
 {
-    in>>s.number>>s.ShopName>>s.CreditWorthiness>>s.goods[0]>>s.goods[1]>>s.goods[2]>>s.goods[3]>>s.goods[4]>>s.goods[5];
-    return in;
+    in>>s.number>>s.ShopName>>s.CreditWorthiness;
+    for (int i = 0; i < goodsNumber; ++i)
+    	in>>s.goods[i];
+	return in;
 }
 ostream& operator<<(ostream& out, const Shop& s)
 {
-    out<<s.number<<" "<<s.ShopName<<" "<<s.CreditWorthiness<<" "<<s.goods; 
-    return out;
+    out<<s.number<<" "<<s.ShopName<<" "<<s.CreditWorthiness<<" "; 
+    for (int i = 0; i < goodsNumber; ++i)
+    	out<<s.goods[i];
+	return out;
 }
 typedef int Status;
 typedef struct Node
@@ -48,7 +54,25 @@ typedef struct Node
     struct Node *next;
 }Node;
 typedef struct Node *LinkList; 
-Status ReadData()
+void DisplayMenu()
+{
+	cout<<"\t\t购物网站信息管理\n\n";
+	cout<<"<1>读取商铺信息\t\t<2>对商铺进行操作\t<3>修改商品价格\n";
+	cout<<"<4>查询商品\t\t<5>购买商品\t\t<6>退出"<<endl; 
+	cout<<"输入选择：";
+}
+int GetLength(LinkList L)	//求链表的长度 
+{
+	LinkList p = L->next;
+	int i = 0;
+	while(p)
+	{
+		i++;
+		p = p->next;
+	}
+	return i;
+}
+LinkList ReadData()
 {
 	fstream fp;
 	fp.open("Shops.txt",ios::in);
@@ -71,15 +95,76 @@ Status ReadData()
 		q = p;	
 	}
 	fp.close();
-	p=L->next;  
-    while(p)  
-    {  
-        cout<<p->data; 
-        p=p->next; 
-    }  
+//	p=L->next;  
+//   	while(p)  
+//   	{  
+//       cout<<p->data<<endl; 
+//       p=p->next; 
+//  	}  
+  	return L;
+}
+void AddShop(LinkList &L)
+{
+	LinkList p,q;
+	p = (LinkList)malloc(sizeof(Node));
+	q = L->next;
+	while(q->next)
+		q = q->next;	//循环到链表
+	int i,num;
+	cout<<"请输入增加的商铺信息\n";
+	i = GetLength(L);
+	i++;
+	p->data.number = i;
+	cout<<"商铺名：";
+	cin>>p->data.ShopName;
+	cout<<"信誉度：";
+	cin>>p->data.CreditWorthiness;
+	cout<<"请输入商品个数：";
+	cin>>num;
+	for(i = 0; i < num; i++) 
+	{
+		cout<<"商品名称：";
+		cin>>p->data.goods[i].ItemsName; 
+		cout<<"商品价格：";
+		cin>>p->data.goods[i].price;
+		cout<<"商品销量："; 
+		cin>>p->data.goods[i].Sales;
+	}
+	q->next = p;
+	p->next = NULL;
+}
+void DeleteShop(LinkList &L)
+{
+	
 }
 int main()
 {
-	ReadData(); 
+	LinkList Shop;
+	int choice;
+	DisplayMenu();
+	cin>>choice;
+	while(choice != 6)
+	{
+		switch(choice)
+		{
+			case 1:Shop = ReadData();cout<<"读取商铺信息完毕!"<<endl;system("pause");system("cls");DisplayMenu();cin>>choice;break;
+			case 2:
+			
+			{
+				cout<<"(1)增加商铺\t\t(2)删除商铺\n输入选择：";
+				cin>>choice;
+				if(choice == 1)
+					AddShop(Shop);
+				if(choice == 2)
+					DeleteShop(Shop);
+				else
+					cout<<"输入有误!\n";
+				system("pause");
+				system("cls");
+				DisplayMenu();cin>>choice;
+				break;
+			}
+		}
+	}
 	return 0;
 }
